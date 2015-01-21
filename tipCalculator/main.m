ceee5b423c4dfda859ca7a468290c252150dc3bf
Ashok Kumar <ashkumar@yahoo-inc.com>
2015-01-20 17:22:08 -0800

    Initial Commit

---
+++ tipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipCalculator
+//
+//  Created by Ashok Kumar on 1/20/15.
+//  Copyright (c) 2015 ashok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
